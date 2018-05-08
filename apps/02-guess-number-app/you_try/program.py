8d95be640121664a7fa0218cdafa33615612497b
tdnvl <bummel1889@gmail.com>
2018-05-08 13:31:47 -0400

    Complete App 2.

--- apps/02-guess-number-app/you_try/program.py
+++ apps/02-guess-number-app/you_try/program.py
@@ -5,5 +5,16 @@ print("     GUESS THAT NUMBER GAME")
 print("----------------------------------")
 
 that_number = random.randint(0, 100)
+game = 1
 
-print(that_number)
+while game == 1:
+    your_guess = input("Guess a number between 1 and 100: ")
+    your_guess = int(your_guess)
+    if your_guess > that_number:
+        print("The number is smaller than {}".format(your_guess))
+    elif your_guess < that_number:
+        print("The number is larger than {}".format(your_guess))
+    else:
+        game = 0
+
+print("Congratulations! You guessed {}!".format(that_number))
