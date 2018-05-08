bb9e45dd18c0d2da290911c68b0e14059887a67c
tdnvl <bummel1889@gmail.com>
2018-05-08 07:05:58 -0400

    Complete app #1 using .format()

--- apps/01_hello_world/you_try/program.py
+++ apps/01_hello_world/you_try/program.py
@@ -2,4 +2,7 @@ print('-------------------------------')
 print('           HELLO APP')
 print('-------------------------------')
 print()
+
 name = input("What is your name? ")
+
+print("Nice to meet you, {}!".format(name))
