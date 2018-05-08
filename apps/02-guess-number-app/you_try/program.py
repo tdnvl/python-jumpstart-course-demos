d0c3d541db74b77c2b4b8d1269d3495801fa96d4
tdnvl <bummel1889@gmail.com>
2018-05-08 07:21:04 -0400

    Start app 2.

---
+++ apps/02-guess-number-app/you_try/program.py
@@ -0,0 +1,9 @@
+import random
+
+print("----------------------------------")
+print("     GUESS THAT NUMBER GAME")
+print("----------------------------------")
+
+that_number = random.randint(0, 100)
+
+print(that_number)
